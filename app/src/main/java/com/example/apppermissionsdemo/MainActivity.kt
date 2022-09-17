124ac702a7a2293e71e3885bd7c0b4ea564288ef
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2022-09-17 20:07:17 +0530

    Permissions for Camera and Location

--- app/src/main/java/com/example/apppermissionsdemo/MainActivity.kt
+++ app/src/main/java/com/example/apppermissionsdemo/MainActivity.kt
@@ -1,11 +1,86 @@
 package com.example.apppermissionsdemo
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private val cameraResultLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val cameraAndLocationResultLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    if (permissionName == Manifest.permission.ACCESS_FINE_LOCATION) {
+                        Toast.makeText(this, "Permission granted for location", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        Toast.makeText(this, "Permission granted for camera", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                } else {
+                    Toast.makeText(this, "Permissions Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnRequestCameraPermission: Button? =
+            findViewById(R.id.btnRequestCameraPermission)
+
+        btnRequestCameraPermission?.setOnClickListener {
+            btnRequestCameraPermission!!.background =
+                getDrawable(R.drawable.button_background_clicked)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)
+            ) {
+                showRationalDialog(
+                    "Camera Permission",
+                    "Your mobile does not support this application"
+                )
+            } else {
+                cameraAndLocationResultLauncher.launch(
+                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION)
+                )
+            }
+        }
+
+    }
+
+    private fun showRationalDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setMessage(message)
+        builder.setTitle(title)
+        builder.setPositiveButton("Yes") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
     }
 }
